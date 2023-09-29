7fb08505db7a25bac3a882a3cdb18520821b3ba7
Densel <esekondensel@gmail.com>
2023-09-29 09:53:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+// this is the array ['Antonia', 'Nuru', 'Amari', 'Mo'] from test.
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  function createFareMultiplier(multiplier) {
+    return function (fare) {
+      return fare * multiplier;
+    };
+  }
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  function selectDifferentDrivers(drivers, driverFunction) {
+    return driverFunction(drivers);
+  }
+  
