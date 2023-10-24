eede0d754a08f174f2f958d233b301adb89dc2f2
densel.esekon2@student.moringaschool.com <esekondensel@gmail.com>
2023-10-24 22:05:46 +0300

    Assignment Resubmitted

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 // Code your solution in this file!
 
 // this is the array ['Antonia', 'Nuru', 'Amari', 'Mo'] from test.
-function returnFirstTwoDrivers(drivers) {
-    return drivers.slice(0, 2);
-  }
-  
-  function returnLastTwoDrivers(drivers) {
-    return drivers.slice(-2);
-  }
-  
-  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-  
-  function createFareMultiplier(multiplier) {
-    return function (fare) {
-      return fare * multiplier;
+// Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+const returnFirstTwoDrivers = (drivers) => {return drivers.slice(0,2)};
+
+console.log (returnFirstTwoDrivers(drivers));
+
+
+const returnLastTwoDrivers = (drivers) => {return drivers.slice(-2)};
+
+
+const selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers]
+
+
+function createFareMultiplier(value){
+    function fareMultiplier(value){
+        return value * value;
     };
-  }
-  
-  const fareDoubler = createFareMultiplier(2);
-  
-  const fareTripler = createFareMultiplier(3);
-  
-  function selectDifferentDrivers(drivers, driverFunction) {
-    return driverFunction(drivers);
-  }
+    return fareMultiplier;
+}
+
+
+function fareDoubler(value){
+    let newfare = value * 2;
+    return newfare;
+}
+
+
+function fareTripler(value){
+    let newfaret = value * 3;
+    return newfaret;
+}
+
+
+function selectDifferentDrivers(value,value2=returnLastTwoDrivers()) {
+    return value2(value);
+}
+
+selectDifferentDrivers(drivers,returnFirstTwoDrivers());
   
